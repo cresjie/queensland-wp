e1d80a8f07310fdfd7d5cc66483aed126dfe47dc
Cres Jie <cresjie@gmail.com>
2016-11-29 10:08:21 -0800

    hotel search

---
+++ single.php
@@ -0,0 +1,19 @@
+
+<?php get_header(); ?>
+
+<div class="container single">
+	<?php while ( have_posts() ) : the_post(); ?>
+		<div class="post-wrapper">
+			<div class="page-title transparent-black spacer-bottom pad-5-10">
+				<h1 class="margin-none post-title"><?php the_title(); ?></h1>
+			</div>
+
+			<div class="transparent-black pad-5-10 post-content">
+				<?php the_content(); ?>
+			</div>
+			
+		</div>
+	<?php endwhile; // end of the loop. ?>
+</div>
+
+<?php get_footer(); ?>
